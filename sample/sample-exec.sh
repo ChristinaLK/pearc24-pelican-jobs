a463486bcbbd120ef314bb164f938e1408b3f06b
Christina Koch <christinakconnect@gmail.com>
2024-07-21 16:14:20 -0400

    tweaks to sample jobs

--- sample/sample-exec.sh
+++ sample/sample-exec.sh
@@ -6,6 +6,6 @@ printf "Job is running on node: "; /bin/hostname
 printf "Job running as user: "; /usr/bin/id
 printf "Job is running in directory: "; /bin/pwd
 
-printf "Contents of $1 is "; cat $1
-cat $1 > output$2.txt
+printf "Contents of input.txt is "; cat input.txt
+cat input.txt > output$1.txt
 sleep 20
